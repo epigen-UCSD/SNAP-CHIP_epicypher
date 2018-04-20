2d03eef30fb8cd033e99ab86e19bd42514b31912
frank@tscc <zhangc518@gmail.com>
2018-04-20 10:28:00 -0700

    add test script;

---
+++ test/run.sh
@@ -0,0 +1,13 @@
+set=$1
+SETDIR="/home/zhc268/data/outputs/setQCs/"
+samplenames=(`cat $SETDIR${set}.txt`)
+
+for s in  ${samplenames[@]};do
+    echo $s
+    time  bash epicypher.sh -i ~/data/seqdata/${s}.fastq.bz2 -m 0 \
+          -o ~/data/outputs/libQCs/${s}/ \
+          1> ~/data/outputs/libQCs/${s}/${s}.fastq.bz2.snap.run.log \
+          2> &1  & sleep 5
+done
+
+wait 
